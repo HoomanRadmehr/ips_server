2cd416bf92dec7a776228c37644f6ab8d15893f4
h.radmehr <h.radmehr>
2023-07-29 10:00:47 +0330

    add csrf

--- ips_server/settings.py
+++ ips_server/settings.py
@@ -222,3 +222,4 @@ if MODE:
         DEBUG=False
         from django.core.management.utils import get_random_secret_key
         SECRET_KEY=get_random_secret_key()
+        CSRF_TRUSTED_ORIGINS=[os.getenv("IPS_SERVER_DOMAIN_NAME")]
